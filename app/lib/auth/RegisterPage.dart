10ccad0c05ea19ec63e3e992dc28e7a1a3b06200
Lalfieri <78233106+Lalfieri@users.noreply.github.com>
2023-03-27 10:53:22 -0400

    Made the UI for the Login and Register Pages. Tried to use flutter spacing stuff.

--- app/lib/auth/RegisterPage.dart
+++ app/lib/auth/RegisterPage.dart
@@ -34,6 +34,9 @@ class _RegisterPage extends State<RegisterPage> {
 
     @override
     Widget build(BuildContext context) {
+        double width = MediaQuery.of(context).size.width;
+        double height = MediaQuery.of(context).size.height;
+        
         return Scaffold(
             appBar: AppBar(
                 backgroundColor: Color(0xFFEC4686),
@@ -43,16 +46,60 @@ class _RegisterPage extends State<RegisterPage> {
                     ),
                 ),
             ),
-            body: Column(
-                children: [
-                    Text("Register"),
-                    ElevatedButton(
-                        child: Text("Register"),
-                        onPressed: () {
-                            createAccount();
-                        }
-                    )
-                ]
+            body: 
+              Container(alignment: Alignment.center,
+              color: Color(0xFFF8D5BC),
+              child:
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:[
+                        SizedBox(
+                          width: height/4, 
+                          height:height/4,
+                          child: Image.asset("assets/logo/logo.png",),
+                          ),
+                        Text("PB & J",style: TextStyle(fontSize: 40,),),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: width/5, vertical: 8),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Username:',
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: width/5, vertical: 8),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Password:',
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: width/5, vertical: 8),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Email:',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                              width: width/3,
+                              height: height/20,
+                              child:
+                                ElevatedButton(
+                                    child: Text("Register"),
+                                    onPressed: () {
+                                        createAccount();
+                                    }
+                                )
+                        ),
+                    ]
+                )             
             )
         );
     }
